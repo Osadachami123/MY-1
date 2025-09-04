b0d33eb50ced60e5a44755a0db185229a82e001b
BLACK MAFIA <100425878+Osadachami123@users.noreply.github.com>
2025-09-04 16:58:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "x70WVY6I#mWAdIg0bvYAQeumHkMk0755eQNLX5MnafrrC-wXQsL8",
+SESSION_ID: process.env.SESSION_ID || "OMdxzZCK#AIWrLyw0IlHjkviNYkUoZARLB0QqGP7OQNqEYG2JpE4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nsNpzrh/Blue-and-White-Modern-The-Future-Of-Artificial-Intelligence-You-Tube-Thumbnail.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Alive",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hey, I'm OSADA MD , created by Osada Chamikara  ✨🍂
+
+I'm always here for help you. 😇 Love you forever ♥️
+
+> ⛓️‍💥 Find and share WhatsApp and Telegram links easily on Linkgic.site.",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 };
